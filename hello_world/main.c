0be3e5e63870b1273dc2cf977fa83c0e0ec9c591
Dylan Serigba <12297@holbertonstudents.com>
2025-10-21 11:31:23 +0200

    Fix script main.c

--- hello_world/main.c
+++ hello_world/main.c
@@ -1,11 +1,8 @@
-#include <stdio.h>
+echo '#include <stdio.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
+    printf("Hello, world\\n");
     return (0);
-}
+}' > main.c
+
