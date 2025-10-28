0a702157381df8863e6baaf13a0c98a5156f7290
Dylan Serigba <12297@holbertonstudents.com>
2025-10-28 10:58:45 +0100

    README.md

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Vérifie si un caractère est une majuscule.
+ * @c: Le caractère à tester.
+ *
+ * Return: 1 si le caractère est une majuscule, 0 sinon.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
