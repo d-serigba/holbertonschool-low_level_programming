7629535006d24c2199b11014ff867c3308b061a6
Dylan Serigba <12297@holbertonstudents.com>
2025-10-29 11:02:11 +0100

    Fix script 0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,13 @@
-#include main.h
+#include <stdio.h>
+#include "main.h"
 
 /**
- * reset_to_98 - met la valeur de l'entier pointé à 98
- * @n: pointeur vers un entier à modifier
- *
- * Description: Cette fonction prend en paramètre un pointeur vers un entier
- * et change la valeur de cet entier pour 98.
- *
- * Return: rien.
+ * reset_to_98 - tkt
+ * @n: pa
+ * Return: nothing
  */
+
 void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
