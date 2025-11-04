f8d9ee713b29849124c626123f4209af1cc54cc8
Dylan Serigba <12297@holbertonstudents.com>
2025-11-04 14:22:54 +0100

    Add script main.h

---
+++ recursion/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Your Name
+ * Desc: Header file containing function prototypes
+ *       for the recursion project.
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif /* MAIN_H */
