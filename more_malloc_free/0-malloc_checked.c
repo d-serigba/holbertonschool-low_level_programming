0a631e95f2c4618fb5005a0a3399ab878422907a
Dylan Serigba <12297@holbertonstudents.com>
2025-11-13 13:18:43 +0100

    Allocating memory using malloc

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * malloc_checked - Allocates memory using malloc
- * @b: Number of bytes to allocate
+ * malloc_checked - Alloue de la mémoire avec malloc
+ * @b: Nombre d’octets à allouer
  *
- * Return: Pointer to the allocated memory
- * If malloc fails, exit with status 98
+ * Return: Pointeur vers la mémoire allouée
+ * Si malloc échoue, termine le programme avec un statut 98
  */
 void *malloc_checked(unsigned int b)
 {
@@ -16,3 +17,22 @@ void *malloc_checked(unsigned int b)
 
     return ptr;
 }
+
+int main(void)
+{
+    int *arr;
+    int i;
+
+    arr = malloc_checked(5 * sizeof(int));
+
+    for (i = 0; i < 5; i++)
+        arr[i] = i + 1;
+
+    for (i = 0; i < 5; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    free(arr);
+    return 0; 
+}
+
