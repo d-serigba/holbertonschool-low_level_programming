bab3a3b218fda92719b61439c3a2a2556f99ae96
Dylan Serigba <12297@holbertonstudents.com>
2025-11-18 11:32:04 +0100

    Function pointers header

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,14 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stddef.h>
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+#endif
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
 void print_name(char *name, void (*f)(char *));
 
 #endif
